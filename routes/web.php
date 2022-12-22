e8efe158dd3d7758d173d525bac09eaaa524bb92
Abizar Valentino Fachri <203040086@main.unpas.ac.id>
2022-12-22 23:15:11 +0700

    Menjalankan fungsi login dan registrasi beserta membuat user

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
 
+// use App\Models\Post;
+// use App\Models\User;
+
+use App\Http\Controllers\AdminCategoryController;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -45,6 +53,51 @@ Route::get('/wanita', function () {
     ]);
 });
 
-Route::get('/login', [LoginController::class, 'index']);
 
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
+
+// halaman single post
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+// Route::get('/categories', function () {
+//     return view('categories', [
+//         'title' => 'Post Categories',
+//         'active' => 'categories',
+//         'categories' => Category::all()
+//     ]);
+// });
+
+// Route::get('/categories/{category:slug}', function(Category $category) {
+//     return view('posts', [
+//         'title' => "Post By Category : $category->name",
+//         'active' => 'categories',
+//         'posts' => $category->posts->load('category', 'author')
+//     ]);
+// });
+
+// Route::get('/authors/{author:username}', function(User $author) {
+//     return view('posts', [
+//         'title' => "Post By Author : $author->name",
+//         'active' => 'posts',
+//         'posts' => $author->posts->load('category', 'author'),
+//     ]);
+// });
+
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
+
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])
+    ->middleware('auth');
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
