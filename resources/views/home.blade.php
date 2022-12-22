e57c7901fc61e0416f26b523077cacbb90ce078e
yghpras <yoghip2@gmail.com>
2022-12-23 04:27:33 +0700

    menambahkan footer & dan memperbaiki halaman home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-<h1 class="row justify-content-center">End Of Season Sale : Shop Now!</h1>
+<h1 class="row justify-content-center py-3">End Of Season Sale</h1>
 <a href="/wanita" style="color:aliceblue">
   <div class="card bg-drak text-white">
 	  <img src="img/yyy.jpg" class="card-img-top" alt="...">
@@ -70,58 +70,55 @@
       <div class="col-md-2">
         <div class="card">
           <img src="img/yyy.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <div class="card-body text-center">
+            <a href="#" style="background-color: black" class="btn btn-secondary btn-block">NIKE <br> Up to 50%</a>
           </div>
         </div>
       </div>
       <div class="col-md-2">
         <div class="card">
           <img src="img/yyy.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <div class="card-body text-center">
+            <a href="#" style="background-color: black" class="btn btn-secondary btn-block">Adidas <br> Up to 50%</a>
           </div>
         </div>
       </div>
       <div class="col-md-2">
         <div class="card">
           <img src="img/yyy.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          <div class="card-body text-center">
+            <a href="#" style="background-color: black" class="btn btn-secondary btn-block">NIKE <br> Up to 50%</a>
           </div>
         </div>
       </div>
       <div class="col-md-2">
         <div class="card">
           <img src="img/yyy.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <div class="card-body text-center">
+            <a href="#" style="background-color: black" class="btn btn-secondary btn-block">NIKE <br> Up to 50%</a>
           </div>
         </div>
       </div>
       <div class="col-md-2">
         <div class="card">
           <img src="img/yyy.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <div class="card-body text-center">
+            <a href="#" style="background-color: black" class="btn btn-secondary btn-block">NIKE <br> Up to 50%</a>
           </div>
         </div>
       </div>
       <div class="col-md-2">
         <div class="card">
           <img src="img/yyy.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <div class="card-body text-center">
+            <a href="#" style="background-color: black" class="btn btn-secondary btn-block">NIKE <br> Up to 50%</a>
           </div>
         </div>
       </div>
     </div>
+
+    
+  </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha383-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
