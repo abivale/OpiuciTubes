4ae148b33781c34bc15f9881805968938c4ebb7a
yghpras <yoghip2@gmail.com>
2022-12-29 19:49:18 +0700

    revisi tabel user

--- database/migrations/2022_12_29_113737_create_posts_table.php
+++ database/migrations/2022_12_29_113737_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
             $table->string('nama_barang');
             $table->string('harga_barang');
             $table->string('category');
