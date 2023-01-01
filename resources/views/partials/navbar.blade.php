8eb38a460816ac16ebc4289f8341da55afbce090
yghpras <yoghip2@gmail.com>
2023-01-01 21:43:41 +0700

    Revisi Semuagit add .

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,67 +1,50 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-black">
+  <div class="container">
+    <a class="navbar-brand" href="/">Bubu's Blog</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link {{ ($active === "home") ? 'active' : ''}}"  href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ ($active === "about") ? 'active' : ''}}" href="/about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ ($active === "posts") ? 'active' : ''}}" href="/posts">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ ($active === "categories") ? 'active' : ''}}" href="/categories">Categories</a>
+        </li>
+      </ul>
 
-   <nav>
-    <header class="p-3 bg-black">
-      <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-          </a>
-    
-          <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="/" class="nav-link px-2 text-white">Home</a></li>
-            <li><a href="/category" class="nav-link px-2 text-white">Category</a></li>
-            
-            <li><a href="/about" class="nav-link px-2 text-white">About</a></li>
-          </ul>
-         
-          <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-            <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
-          </form>
-          
+      <ul class="navbar-nav ms-auto">
           @auth
-          <li class="nav-item dropdown row">
-            <a class="nav-link dropdown-toggle px2 text-white" href="/" role="button" data-bs-toggle="dropdown">
-              Welcome Back {{ auth()->user()->name }}
-            </a>
-            <ul class="dropdown-menu bg-drack">
-              <li><a class="dropdown-item text-black" href="/dashboard"><i class="bi bi-layout-text-window-reverse"></i> My Dashboard</a></li>
-              <li>
-                 <form action="/logout" method="post">
-                  @csrf
-                  <button type="submit" class="dropdown-item text-black"><i class="bi bi-box-arrow-in-right"></i> Logout</a></button>
-                 </form>
-              </li>
-            </ul>
-          </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Welcome back, {{ auth()->user()->name }}
+              </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="/dashboard">My Dashboard</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li>
+              <form action="/logout" method="post">
+                @csrf
+                <button type="submit" class="dropdown-item">Logout</button>
+              </form>
+            </li>
+          </ul>
+        </li>
           @else
-          <div class="text-end">
-            <a href="/login"><button type="button" class="btn btn-outline-light me-2">Login</button></a>
-            <a href="/register"><button type="button" class="btn btn-warning">Buat Akun</button></a>
-            
-          </div>
+          <li class="nav-item">
+            <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : ''}}">Login</a>
+          </li>
           @endauth
-          
-        </div>
-      </div>
-    </header>
-   </nav>
-   <div class="d-flex">
-    <div class="form-check form-switch ms-auto mt-3 me-3 py-0">
-      <label class="form-check-label ms-3" for="lightSwitch">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="25"
-          height="25"
-          fill="currentColor"
-          class="bi bi-brightness-high"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"
-          />
-        </svg>
-      </label>
-      <input class="form-check-input" type="checkbox" id="lightSwitch" />
+        </ul>
+      
+
     </div>
   </div>
-  
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Models\Post;
-use App\Models\User;
+// use App\Models\Post;
+// use App\Models\User;
 
-
+use App\Http\Controllers\AdminCategoryController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardPostController;
@@ -24,46 +25,32 @@ use App\Http\Controllers\DashboardPostController;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "home",
-        "posts" => Post::all()
+        "title" => "Home",
+        'active' => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
-        "title" => "About"
+        "title" => "About",
+        "active" => "About"
     ]);
-});
+}); 
 
-Route::get('/category', function () {
-    return view('category', [
+
+
+Route::get('/posts', [PostController::class, 'index']);
+
+// halaman single post
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function() {
+    return view('categories', [
         'title' => 'Post Categories',
         'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
-
-
-
-Route::get('/erigo', function () {
-    return view('erigo', [
-        "title" => "Erigo"
-    ]);
-});
-
-Route::get('/hecate', function () {
-    return view('hecate', [
-        "title" => "Hecate"
-    ]);
-});
-Route::get('/ventela', function () {
-    return view('ventela', [
-        "title" => "Ventela"
-    ]);
-});
-
-
-
 
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
@@ -72,9 +59,13 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-
-Route::get('dashboard', function() {
+Route::get('/dashboard', function() {
     return view('dashboard.index');
 })->middleware('auth');
 
-Route::resource('/dashboard/post', DashboardPostController::class)->middleware('auth');
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])
+->middleware('auth');
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
