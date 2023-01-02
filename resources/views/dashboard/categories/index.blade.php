b6b9cdd40292ad3fccbaebf2051caec4104bf792
yghpras <yoghip2@gmail.com>
2023-01-03 00:20:31 +0700

    Revisi

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-  <h1 class="h2">Post Categories</h1>
+  <h1 class="h2">Kategori Produk </h1>
 </div>
 
 @if(session()->has('success'))
@@ -13,12 +13,12 @@
 
 
 <div class="table-responsive col-lg-6">
-  <a href="/dashboard/categories/create" class="btn btn-primary mb-3">Create new category</a>
+  <a href="/dashboard/categories/create" class="btn btn-primary mb-3">Buat kategori baru</a>
         <table class="table table-striped table-sm">
           <thead>
             <tr>
               <th scope="col">#</th>
-              <th scope="col">Category Name</th>
+              <th scope="col">Nama Kategori</th>
               <th scope="col">Action</th>
             </tr>
           </thead>
